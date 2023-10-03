50a8c894d0e4dbf6724cacecc41e13a56a244412
Fuzail Danish Khan <117436474+Zak455@users.noreply.github.com>
2023-10-04 00:07:22 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is the first rule of surviving a zombie apocalypse?",
+    answer: "Cardio",
+    options: [
+      "Don't panic",
+      "Cardio",
+      "Stockpile food",
+      "Find a safe house"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What weapon is often recommended for dispatching zombies?",
+    answer: "Firearm",
+    options: [
+      "Baseball bat",
+      "Firearm",
+      "Crowbar",
+      "Machete"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What should you aim for when attacking a zombie?",
+    answer: "Head",
+    options: [
+      "Legs",
+      "Arms",
+      "Chest",
+      "Head"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is the best way to travel during a zombie outbreak?",
+    answer: "Quietly",
+    options: [
+      "Quickly",
+      "Noisy",
+      "With a group",
+      "Quietly"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What should you do if you get bitten by a zombie?",
+    answer: "Amputate the bitten limb",
+    options: [
+      "Take antibiotics",
+      "Amputate the bitten limb",
+      "Ignore it",
+      "Seek medical attention"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the most important resource in a zombie apocalypse?",
+    answer: "Water",
+    options: [
+      "Gold",
+      "Weapons",
+      "Water",
+      "Fuel"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the best strategy for fortifying your shelter?",
+    answer: "Barricade all entrances",
+    options: [
+      "Hide quietly",
+      "Barricade all entrances",
+      "Build a moat",
+      "Set traps"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What time of day are zombies most active?",
+    answer: "Night",
+    options: [
+      "Morning",
+      "Day",
+      "Dusk",
+      "Night"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is the first thing you should do when encountering a zombie?",
+    answer: "Assess the situation",
+    options: [
+      "Scream and run",
+      "Shoot it",
+      "Assess the situation",
+      "Offer it food"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the best way to attract zombies away from your location?",
+    answer: "Make noise",
+    options: [
+      "Stay quiet",
+      "Make noise",
+      "Use bait",
+      "Set a fire"
+    ]
+  }
+];
